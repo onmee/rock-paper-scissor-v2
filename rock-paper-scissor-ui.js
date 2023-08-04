65cd85ad8367a508ead7bcceace1795f3021acb3
onmee <48259011+onmee@users.noreply.github.com>
2023-08-04 16:16:31 +0100

    Create object to store scores
    Modified playRound to update scores

--- rock-paper-scissor-ui.js
+++ rock-paper-scissor-ui.js
@@ -1,5 +1,6 @@
 
 const GAME_CHOICES = ['rock', 'paper', 'scissor'];
+const scores =  {Player: 0, Computer: 0};
 
 // Randomly returns 'Rock', 'Paper' or 'Scissor' for the computer.
 function getComputerChoice() {
@@ -10,36 +11,42 @@ function getComputerChoice() {
 // Event listener for each of the choice buttons.
 const choiceBtns = document.querySelectorAll(".choice-btns button");
 choiceBtns.forEach((btn => btn.addEventListener('click', btn => {
-    let playerChoice = btn.target.textContent.toLowerCase();
-    playRound(playerChoice, getComputerChoice());
+  let playerChoice = btn.target.textContent.toLowerCase();
+  playRound(playerChoice, getComputerChoice());
+  console.log(scores)
 })))
 
 
 // Plays one round of the game, check the outcome, return the outcome text and scores in array.
-// Choices must be renewed every round; so the choice functions are the parameters here.
 // Replayed if both players have the same choice, as only wins are counted.
 function playRound(player, computer) {
-    console.log(player, computer)
-    if (computer == player) {
-      alert(`Both chose ${computer}, replay round`);
-      return;
-    }
-    else if (computer == 'rock' && player == 'scissor') {
-      return ["Computer won as rock beats scissor", 1, 0]
-    }
-    else if (computer == 'rock' && player == 'paper') {
-      return ["You win as paper beats rock", 0, 1];
-    }
-    else if (computer == 'scissor' && player == 'rock') {
-      return ["You win as rock beats scissor", 0, 1];
-    }
-    else if (computer == 'scissor' && player == 'paper') {
-      return ["Computer won as scissor beats paper", 1, 0];
-    }
-    else if (computer == 'paper' &&  player == 'scissor') {
-      return ["You won as scissor beats paper", 0, 1];
-    }
-    else if (computer == 'paper' &&  player == 'rock') {
-      return ["Computer won as paper beats rock", 1, 0];
-    }
+  console.log(player, computer)
+  if (player == computer) {
+    alert(`Both chose ${computer}, replay round`);
+    return;
   }
+  else if (computer == 'rock' && player == 'scissor') {
+    ++ scores.Computer;
+    return "Computer won as rock beats scissor"
+  }
+  else if (computer == 'rock' && player == 'paper') {
+    ++ scores.Player;
+    return "You win as paper beats rock";
+  }
+  else if (computer == 'scissor' && player == 'rock') {
+    ++ scores.Player;
+    return "You win as rock beats scissor";
+  }
+  else if (computer == 'scissor' && player == 'paper') {
+    ++ scores.Computer;
+    return "Computer won as scissor beats paper";
+  }
+  else if (computer == 'paper' &&  player == 'scissor') {
+    ++ scores.Player;
+    return "You won as scissor beats paper";
+  }
+  else if (computer == 'paper' &&  player == 'rock') {
+    ++ scores.Computer;
+    return "Computer won as paper beats rock";
+  }
+}
