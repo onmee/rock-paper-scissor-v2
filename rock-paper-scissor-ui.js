71f4dba8f16f80bd8fe8fa43a6daf84ad5d3c6e9
onmee <48259011+onmee@users.noreply.github.com>
2023-08-08 14:00:29 +0100

    Move scores and text outputs to another function

--- rock-paper-scissor-ui.js
+++ rock-paper-scissor-ui.js
@@ -15,33 +15,12 @@ choiceBtns.forEach(btn => btn.addEventListener('click', onClick));
 function onClick(btn) {
   gameData.playerChoice = btn.target.textContent.toLowerCase();
   runGame();
-  //console.log(btn.target.textContent);
 }
 
-//choiceBtns.forEach((btn => btn.addEventListener('click', btn => {
-  //gameData.playerChoice = btn.target.textContent.toLowerCase();
-  //runGame();
-  //playRound(playerChoice, getComputerChoice());
 
-  // Update the text and values for the 'Result' and Score cards respectively. (Remove text outputs to another function or runGame)
-  //const resultOutput = document.getElementById('result-output');
-  //resultOutput.textContent = gameData.Result;
-
-  //const playerScore = document.getElementById('player-score');
-  //playerScore.textContent = `${gameData.Player}`;
-
-  //const computerScore = document.getElementById('computer-score');
-  //computerScore.textContent = `${gameData.Computer}`;
-
-  //runGame();
-  
-//})))
-
-
-// Run the game until 5 rounds and then display the relevant message, remove eventListeners and
+// Run the game until 5 wins and then display the relevant message, remove eventListeners and
 // and show a reset button. 
 function runGame() {
-
   playRound(gameData.playerChoice, getComputerChoice());
 
   if (gameData.Player == 5) {
@@ -53,28 +32,13 @@ function runGame() {
     const winMessage = document.getElementById('winner');
     winMessage.textContent = 'Commiserations. You\'ve lost \u{1F641}';
     choiceBtns.forEach(btn => btn.removeEventListener('click', onClick));
-
-  } 
-
-  const resultOutput = document.getElementById('result-output');
-  resultOutput.textContent = gameData.Result;
-
-  const playerScore = document.getElementById('player-score');
-  playerScore.textContent = `${gameData.Player}`;
-
-  const computerScore = document.getElementById('computer-score');
-  computerScore.textContent = `${gameData.Computer}`;
+  }
+  textOutput(); 
 }
-
-// Append reset button to final outcome section.
-
-
-
 
 // Plays one round of the game, check the outcome, return the outcome text and scores in array.
 // Replayed if both players have the same choice, as only wins are counted.
 function playRound(player, computer) {
-
   // Update the text in the 'Computer' card.
   const compOutput = document.getElementById('computer-choice');
   compOutput.textContent = computer.toUpperCase();
@@ -102,4 +66,18 @@ function playRound(player, computer) {
   else if (computer == 'paper' &&  player == 'rock') {
     ++ gameData.Computer; gameData.Result = 'Paper beats rock. Computer wins!';
   }
+}
+
+// Append reset button to final outcome section.
+
+// Scores and results card text outputs.
+function textOutput() {
+  const resultOutput = document.getElementById('result-output');
+  resultOutput.textContent = gameData.Result;
+
+  const playerScore = document.getElementById('player-score');
+  playerScore.textContent = `${gameData.Player}`;
+
+  const computerScore = document.getElementById('computer-score');
+  computerScore.textContent = `${gameData.Computer}`;
 }
